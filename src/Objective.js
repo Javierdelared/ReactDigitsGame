96c939c27ca191b2bc86abdedf03a8f14ae06c45
Javier <javi.red.calvo@gmail.com>
2024-03-05 19:08:24 +0100

    Add objective

--- src/Objective.js
+++ src/Objective.js
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import classNames from 'classnames';
-
 export function Objective({ value }) {
   return <div className='objective-div'><div className='objective'>{value}</div></div>;
 }
